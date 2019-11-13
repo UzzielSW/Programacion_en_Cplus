3beeb88b8788fcad777772d79f4d4788c30fce14
Wardell <brayanpuyol@gmail.com>
2019-11-12 20:30:11 -0500

    1. Modulo 2.5 "Archivos"

---
+++ Modulos de C++/Bloque 2.5 - Archivos/Ejercicio1 - Escritura en un archivo.cpp
@@ -0,0 +1,40 @@
+/*Ejercicio 1: Realice un programa que pida al usuario el nombre de un fichero 
+de texto y, a continuación permita almacenar al usuario tantas frases como el
+usuario desee.*/
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+using namespace std;
+
+void escribirFrases();
+
+int main()
+{
+	escribirFrases();
+	
+	system("pause");
+	return 0;
+} 
+
+void escribirFrases()
+{
+	int edades[5];
+	ofstream archivo;
+
+	archivo.open("archivo.dat", ios::out); //Creamos el archivo
+	
+	if(archivo.fail())
+	{ //Si a ocurrido algun error
+		cout<<"No se pudo abrir el archivo";
+		exit(1);
+	}
+
+	cout<<"ingrese edades: ";
+	for(int i=0; i < 5; i++)
+		cin>>edades[i];
+
+	for (int i = 0; i < 5; i++)
+			archivo <<edades[i]<<" ";
+	
+	archivo.close();
+}
